51f895e244c80334fb9b044fa398d11499f1bcd1
freerunner73 <saifullahkhaliq@gmail.com>
2018-12-15 11:45:08 +0500

    Saif's Commit

--- app/src/main/java/com/example/abdul/spirit/CreateTeam/CreateTeam.java
+++ app/src/main/java/com/example/abdul/spirit/CreateTeam/CreateTeam.java
@@ -1,18 +1,51 @@
 package com.example.abdul.spirit.CreateTeam;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 
 import com.example.abdul.spirit.R;
+import com.example.abdul.spirit.Utils.SinglePlayerViewItemAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateTeam extends AppCompatActivity {
+
+
+    private Button addMemberButton;
+    private ListView addPlayersListView;
+    private SinglePlayerViewItemAdapter addPlayersAdapter;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_team);
+
+        addPlayersListView = findViewById(R.id.addPlayerslist);
+        addMemberButton = findViewById(R.id.addMemberButton);
+
+        List<AddPlayer> addPlayersList = new ArrayList<>();
+        addPlayersAdapter = new SinglePlayerViewItemAdapter(getApplicationContext(),addPlayersList);
+
+        addPlayersListView.setAdapter(addPlayersAdapter);
+
+
+        addMemberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),AddPlayerActivity.class);
+
+            }
+        });
+
+
     }
 }
